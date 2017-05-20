27b6395648923f26777423c8c074c2109bb0f34a
salman128 <salman86@gmail.com>
2017-05-20 17:44:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# District Heating Model 1.0
+# 
+# Salman Siddiqui ucqbsid@ucl.ac.uk 
+# ---------------------------------------
+
+from math import sin
+
+print('District Heating Model 1.0') # test string
+
+# Variables
+a = sin(1.0)
+print((a))
